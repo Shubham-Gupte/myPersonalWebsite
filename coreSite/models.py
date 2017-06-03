bbbf625b0e5414a3dd79742f6cfb8b0c045649ee
SylvrG <shubhamgupte01@gmail.com>
2017-06-02 20:23:56 -0400

    Created Django models

--- coreSite/models.py
+++ coreSite/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
+from multiselectfield import MultiSelectField
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', blank=True)
+    title = models.CharField(max_length=200, blank=True)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+    image = models.FileField()
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+class Project(models.Model):
+    MY_CHOICES2 = ((1, 'Hardware'),
+               (2, 'Software'),
+               (3, 'Both'))
+
+    author = models.ForeignKey('auth.User', blank=True)
+    title = models.CharField(max_length=200, blank=True)
+    text = models.TextField()
+    HorS = MultiSelectField(choices=MY_CHOICES2,
+                                 max_choices=1,
+                                 max_length=3)
+    image = models.FileField()
+    repoLink = models.CharField(max_length=200, blank=True)
+    link = models.CharField(max_length=200, blank=True)
