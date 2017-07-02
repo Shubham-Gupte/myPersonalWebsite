9884fa0613bd4e9559023b861c9de90db6c901cd
Shubham-Gupte <shubhamgupte01@gmail.com>
2017-07-02 02:17:42 -0400

    Finished Contact page with Danny's suggestions

--- coreSite/forms.py
+++ coreSite/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 class ContactForm(forms.Form):
-    subject = forms.CharField(label="Subject", max_length=100,)
-    message = forms.CharField(label="Message", widget=forms.Textarea)
-    sender = forms.EmailField(label="Your Name")
+    subject = forms.CharField(label="Subject", max_length=100,initial="What should we discuss?")
+    message = forms.CharField(label="Message", widget=forms.Textarea, initial="What is your message?")
+    sender = forms.EmailField(label="Your Name", initial="What is your email?")
