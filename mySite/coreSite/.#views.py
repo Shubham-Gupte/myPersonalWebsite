6cc372701d4708265d3b1b09a0a52cbe3efa58b2
Shubham-Gupte <shubhamgupte01@gmail.com>
2017-10-03 20:47:53 -0400

    Added blog to index

--- mySite/coreSite/.#views.py
+++
@@ -1,1 +0,0 @@
-shubhamgupte@lawn-128-61-28-210.lawn.gatech.edu.2939
--- mySite/coreSite/views.py
+++ mySite/coreSite/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils import timezone
+from blog.models import Post
 # Create your views here.
 def home(request):
-    return render(request, 'coreSite/index.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    posts = posts[0]
+    return render(request, 'coreSite/index.html', {'posts':posts})
 def moreAbout(request):
     return render(request, 'coreSite/about.html', {})
